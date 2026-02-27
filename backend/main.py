68c99a07aa691a7ccd0bcf25f70e53a1b28f9ba3
AHoover749 <151232521+AHoover749@users.noreply.github.com>
2026-02-27 10:15:39 -0600

    Added security.py with datetime and bcrypt modified main.py to allow local hosting

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware( 
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+#Allows front end and backend to host locally, will be changed later when we find where we are hostings.
 
 @app.get("/") # This is a decorator for HTML GET. Just "/" is the base page
 def root():
